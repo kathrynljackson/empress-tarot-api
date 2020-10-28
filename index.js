5c4257659843ebd158195ca681fd45650c066b29
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-10-28 19:42:57 -0400

    Add cors package

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+const bodyParser = require('body-parser');
+
 const express = require('express');
 const app = express();
+
 const importData = require("./data.json");
 let port = process.env.PORT || 3000;
+
+const cors = require('cors');
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Empress Tarot api RUNNING');
