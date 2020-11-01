d0c86d2fcc26e5b72d66b5f619b3e7268c35d4f5
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-11-01 17:58:14 -0500

    Make API route dynamic

--- index.js
+++ index.js
@@ -15,7 +15,26 @@ app.get('/', (req, res) => {
 
 app.get('/cards', (req, res) => {
     res.send(importData);
+    //console.log('app', app);
+    console.log('importData', importData);
+    consolelog('data', data)
 });
+
+app.get('/cards/:id', (req, res) => {
+    res.send(importData[req.params.id]);
+});
+
+// app.get('/cards/:id', (req, res) => {
+//     const id = req.params;
+//     const card = importData.find(card => {
+//         return card.id ===id
+//     })
+//     res.send(importData[card.id])
+// });
+
+//The above funcation needs to be refactored. Right now, it is finding cards based 
+//on their index. This works fine for now, but may not be the best choice in case
+//the card API is edited and ever made to be out of order. 
 
 app.listen(port, () => {
     console.log(`Empress Tarot api is listening on port http://localhost:${port}`);
